1ad142c2e6a3736a875366796a72890127810f90
Victor Rudenco <Victor.Rudenco@wu.com>
2019-06-10 23:57:10 +0300

    Infinite scroll completely implemented

--- src/components/app.js
+++ src/components/app.js
@@ -7,7 +7,7 @@ import {
 import axios from "axios";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faTrash, faSignOutAlt , faEdit } from "@fortawesome/free-solid-svg-icons";
+import { faTrash, faSignOutAlt , faEdit,faSpinner } from "@fortawesome/free-solid-svg-icons";
 
 
 import PortfolioContainer from "./portfolio/portfolio-container";
@@ -22,7 +22,7 @@ import PortfolioDetail from  "./portfolio/portfolio-detail";
 import Auth from  "./pages/auth";
 import NoMatch from "./pages/no-match";
 
-library.add(faTrash, faSignOutAlt, faEdit);
+library.add(faTrash, faSignOutAlt, faEdit, faSpinner);
 
 export default class App extends Component {
 
--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import axios from "axios";
 import BlogItem from "../blog/blog-item"
 
@@ -9,25 +10,47 @@ export default class Blog extends Component {
         super();
 
         this.state = {
-            blogItems: []
+            blogItems: [],
+            totalCount: 0,
+            currentPage: 0,
+            isLoading: true
         }
 
         this.getBlogItems = this.getBlogItems.bind(this);
-        this.activateInfiniteScrol();
+        this.activateInfiniteScroll();
     }
 
-    activateInfiniteScrol() {
+    activateInfiniteScroll() {
         window.onscroll = () => {
-            console.log("onscroll");
+            if(this.state.isLoading || 
+                this.state.blogItems.length === this.state.totalCount
+            ) {
+                return;
+            }
+            if (window.innerHeight + parseInt(document.documentElement.scrollTop) === document.documentElement.offsetHeight -1) {
+                console.log("Reached bottom of the page");
+                this.getBlogItems();
+            }
         }
     }
 
     getBlogItems() {
-        axios.get("https://viteok.devcamp.space/portfolio/portfolio_blogs", 
-            {withCredentials: true
-        }).then(response => {
+        this.setState({
+            currentPage: this.state.currentPage + 1
+        });
+        console.log("current state = " , this.state.currentPage);
+
+        axios.get(
+            `https://viteok.devcamp.space/portfolio/portfolio_blogs?page=${this.state.currentPage}`, 
+            {withCredentials: true  }
+            ).then(response => {
+            console.log("getting",response.data);
             this.setState({
-                blogItems: response.data.portfolio_blogs
+                blogItems: this.state.blogItems.concat(
+                    response.data.portfolio_blogs
+                ) ,
+                totalCount:  response.data.meta.total_records,
+                isLoading: false
             })
         }).catch(error => {
             console.log("getBlogItems error", error);
@@ -48,6 +71,11 @@ export default class Blog extends Component {
             <div className="content-container">
                 {blogRecords}
             </div>
+            {this.state.isLoading ? (
+                <div className="content-loader">
+                    <FontAwesomeIcon icon="spinner" spin />    
+                </div>) : null
+            }                
             </div>
         );
     }
