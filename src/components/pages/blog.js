13f6ac309431686e039abb0122929b1944b29c69
Victor Rudenco <Victor.Rudenco@wu.com>
2019-06-11 10:10:07 +0300

    Started Modal

--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -2,7 +2,8 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import axios from "axios";
-import BlogItem from "../blog/blog-item"
+import BlogItem from "../blog/blog-item";
+import BlogModal from "../modals/blog-modal";
 
 export default class Blog extends Component {
 
@@ -13,24 +14,39 @@ export default class Blog extends Component {
             blogItems: [],
             totalCount: 0,
             currentPage: 0,
-            isLoading: true
+            isLoading: true,
+            blogModalIsOpen: false
+
         }
 
         this.getBlogItems = this.getBlogItems.bind(this);
-        this.activateInfiniteScroll();
+        this.onScroll= this.onScroll.bind(this);
+        window.addEventListener("scroll", this.onScroll, false);
+        this.handleNewBlogClick = this.handleNewBlogClick.bind(this);
+        this.handleModalClose = this.handleModalClose.bind(this);
     }
 
-    activateInfiniteScroll() {
-        window.onscroll = () => {
-            if(this.state.isLoading || 
-                this.state.blogItems.length === this.state.totalCount
-            ) {
-                return;
-            }
-            if (window.innerHeight + parseInt(document.documentElement.scrollTop) === document.documentElement.offsetHeight -1) {
-                console.log("Reached bottom of the page");
-                this.getBlogItems();
-            }
+    handleModalClose() {
+        this.setState({
+            blogModalIsOpen: false
+        });
+    }
+
+    handleNewBlogClick() {
+        this.setState({
+            blogModalIsOpen: true
+        });
+    }
+
+    onScroll() {
+        if(this.state.isLoading || 
+            this.state.blogItems.length === this.state.totalCount
+        ) {
+            return;
+        }
+        if (window.innerHeight + parseInt(document.documentElement.scrollTop) === document.documentElement.offsetHeight -1) {
+            console.log("Reached bottom of the page");
+            this.getBlogItems();
         }
     }
 
@@ -61,6 +77,10 @@ export default class Blog extends Component {
         this.getBlogItems();
     }
 
+    componentWillUnmount() {
+        window.removeEventListener("scroll", this.onScroll, false);
+    }
+
     render() {
         const blogRecords = this.state.blogItems.map(blogItem => {
             return <BlogItem key={blogItem.id} blogItem={blogItem} />;
@@ -68,14 +88,24 @@ export default class Blog extends Component {
 
         return (
             <div className="blog-container">
-            <div className="content-container">
-                {blogRecords}
-            </div>
-            {this.state.isLoading ? (
-                <div className="content-loader">
-                    <FontAwesomeIcon icon="spinner" spin />    
-                </div>) : null
-            }                
+                <BlogModal 
+                    handleModalClose={this.handleModalClose}
+                    modalIsOpen={this.state.blogModalIsOpen}/>
+
+                <div className="new-blog-link">
+                <a onClick={this.handleNewBlogClick}>
+                    Open Modal !
+                </a>
+                </div>
+
+                <div className="content-container">
+                    {blogRecords}
+                </div>
+                {this.state.isLoading ? (
+                    <div className="content-loader">
+                        <FontAwesomeIcon icon="spinner" spin />    
+                    </div>) : null
+                }                
             </div>
         );
     }
