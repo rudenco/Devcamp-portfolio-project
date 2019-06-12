2650cffa04f80b0912600ed4926b5c43807886c9
Victor Rudenco <Victor.Rudenco@wu.com>
2019-06-12 20:36:30 +0300

    Started Draft js

--- src/components/app.js
+++ src/components/app.js
@@ -7,8 +7,6 @@ import {
 import axios from "axios";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faTrash, faSignOutAlt , faEdit,faSpinner,faPlusCircle } from "@fortawesome/free-solid-svg-icons";
-
 
 import PortfolioContainer from "./portfolio/portfolio-container";
 import NavigationContainer from "./navigation/navigation-container";
@@ -21,13 +19,15 @@ import PortfolioManager from "./pages/portfolio-manager";
 import PortfolioDetail from  "./portfolio/portfolio-detail";
 import Auth from  "./pages/auth";
 import NoMatch from "./pages/no-match";
+import Icons from "../helpers/icons";
 
-library.add(faTrash, faSignOutAlt, faEdit, faSpinner, faPlusCircle);
 
 export default class App extends Component {
 
   constructor(props) {
     super(props);
+
+    Icons();
 
     this.state = {
       loggedInStatus : "NOT_LOGGED_IN"
@@ -127,7 +127,13 @@ export default class App extends Component {
               <Route path="/about-me" component={About} />
               <Route path="/contact" component={Contact} />
 
-              <Route path="/blog" component={Blog} /> 
+              <Route path="/blog" 
+                render={props => (
+                  <Blog {...props} loggedInStatus={this.state.loggedInStatus} />
+                )} 
+              
+              /> 
+
               <Route path="/b/:slug" component={BlogDetail} /> 
 
               {this.state.loggedInStatus === "LOGGED_IN" ?  (
