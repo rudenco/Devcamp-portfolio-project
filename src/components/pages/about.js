84a2bb2a31d1a6cb7da7ea91a5f17a77b5f6f723
Victor Rudenco <Victor.Rudenco@wu.com>
2019-07-01 11:43:43 +0300

    Upgraded eslint and webpack-dev-server

--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -21,8 +21,9 @@ export default class About extends Component {
                   
                 </div>   
                 <div className="right-column"> 
-                What is Lorem Ipsum?
-                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. Why do we use it? It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+                I am a software tester currently learning and advancing in the area of software development . 
+                Devcamp course gave me the possibilitiy to feel confident with HTML , CSS, SCSS, REACT , Python, Git and other technologies used to work with in this sphere. 
+                I am willing to become a full stack developer.   
                </div>  
             </div>
         );
